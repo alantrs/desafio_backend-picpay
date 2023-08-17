79970a3aac44282f9b657bebb4512b31b88517ef
Alan Araujo Rodrigues <88983038+alantrs@users.noreply.github.com>
2023-08-16 23:53:16 -0300

    Initial commit

---
+++ src/main/java/com/desafiopicpay/DesafiopicpayApplication.java
@@ -0,0 +1,13 @@
+package com.desafiopicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiopicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiopicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiopicpay/DesafiopicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiopicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiopicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
