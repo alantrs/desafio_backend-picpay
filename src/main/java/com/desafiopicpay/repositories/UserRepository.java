f24f61ed22237fba1f430bccca9ebcbad4de3cba
Alan Araujo Rodrigues <88983038+alantrs@users.noreply.github.com>
2023-08-17 00:27:25 -0300

    criado UserRepository

---
+++ src/main/java/com/desafiopicpay/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.desafiopicpay.repositories;
+
+import com.desafiopicpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
